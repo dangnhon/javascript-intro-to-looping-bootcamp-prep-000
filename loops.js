735fa8d474b5c93126d863352e0a04694cf1b959
Nhon Dang <dang.nhon12345@yahoo.com>
2020-08-29 17:56:55 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -22,12 +22,16 @@ function whileLoop(n) {
   return("done");
 }
 
-function doWhileLoop(num) {
-  let i = 0;
-  function incrementVariable() {
-  i = i + 1;
-  
+
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
   }
+
+  do {
+    array.pop();
+  } while (array.length > -1 && maybeTrue());
+    return array;
 }
 
 
